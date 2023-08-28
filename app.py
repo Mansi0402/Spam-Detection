273a8f5648379e0d6d6faddea06a02d9e0355b6e
Mansi0402 <mansiragh@gmail.com>
2023-08-28 15:19:02 +0530

    final commit

--- app.py
+++ app.py
@@ -1,1 +1,58 @@
-print("Hello")
+import streamlit as st;
+import pickle as pk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+import string
+
+
+def transform_text(text):
+    ps = PorterStemmer()
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    
+    y = []
+    
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        if i not in stopwords.words('english'):
+            y.append(i)
+            
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(ps.stem(i))
+            
+    return " ".join(y)
+
+tfidf = pk.load(open('vectorizer.pkl','rb'))
+model = pk.load(open('model.pkl','rb'))
+
+st.title("Spam Message Classifer")
+
+imput_sms = st.text_area("Enter message")
+
+if st.button("Predict"):
+
+    # 1. Preprocess
+    transformed_text = transform_text(imput_sms)
+
+    # 2. Vectorize
+    vector_input = tfidf.transform([transformed_text])
+
+    # 3. Predict
+    result = model.predict(vector_input)[0]
+
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
